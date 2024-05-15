122eee5f36261133717cf0d629cacae77f578997
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2024-05-15 10:29:20 -0300

    Cancel alll and do it agains

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Create Bucket List array//
+const bucketList = [1, 2 ,3 ,4 ,5, 6 ,7,8 ,9 ,10 ,11 ,12 ,13 ,14 ,15 ,16];
 //Create Hash class//
 class HashMap{
     constructor(key,value){
@@ -10,10 +12,23 @@ class HashMap{
         for(let i = 0 ; i < key.length ; i++ ){
             hashCode = primeNumber * hashCode + key.charCodeAt(i)
         }
-        return hashCode
+        const hashIndex = hashCode % 16
+        const index = hashIndex
+        bucketList.splice(index,1)
+        const newBuckets = [
+            ...bucketList.slice(0, index),
+            hashCode,
+            ...bucketList.slice(index)
+        ];
+        return newBuckets
     }
+    set(key,value){
+        let newHash = new HashMap (key,value)
+        return newHash
+    }
+
 };
 
 
 const escola = new HashMap("toninho","tornado");
-console.log(escola.hash("heitor"))
+console.log(escola.set("toninho","tornado"))
