613fa0c3e45fa0be674150a17e00ded518cd3ad6
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2024-05-15 09:08:49 -0300

    create stringtoCode method

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ class HashMap{
         this.value = value;
     };
     hash(key){
-        let chave = this.key.charCodeAt(0)
-        return chave;
+        let hashCode = 0;
+        const primeNumber = 11;
+        for(let i = 0 ; i < key.length ; i++ ){
+            hashCode = primeNumber * hashCode + key.charCodeAt(i)
+        }
+        return hashCode
     }
 };
 
 
 const escola = new HashMap("toninho","tornado");
-console.log(escola.hash())
+console.log(escola.hash("heitor"))
